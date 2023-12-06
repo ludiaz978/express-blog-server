e2ea0dc693ef9893c4e05d20e140c8220c1d1bb2
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-12-05 17:36:48 -0800

    12.5.23- edit post

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-const BASE_URL ="http://localhost:3001"
-function App() {
-  //create state variable to store all variable posts
-  const [posts, setPosts] = useState([]);
- //create state variable to store data for a new post
-  const [newPost, setNewPost] = useState({id:0, title: "", content: "", comments: []});
-  //use ueseEffect to use axios to fetch all blog posts on page load
-  const[editingPost, setEditingPost ] = useState(null);
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3001'
+
+const App = () => {
+  // create state variable to sore all available posts
+  const [posts, setPosts]= useState([]);
+  // create state variable to store data for a new post
+  const [newPost, setNewPost]= useState({id: 0, title: '', content: '', comments: []});
+  const [editingPost, setEditingPost] = useState(null);
+
+  // use useEffect to use axios to fetch all blog posts on page load
   useEffect(() => {
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}/posts`);
-      setPosts(response.data);
-    } catch (error) {
-        console.error('Error fetching posts:', error);
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/posts`);
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts: ', error);
       }
     };
     fetchPosts();
-  }, []); // only want effect to run when page loads the first time
+  }, [])
+
+  // create delete function
   const handleDelete = async (postId) => {
 		try {
 			await axios.delete(`${BASE_URL}/posts/${postId}`);
@@ -29,48 +34,64 @@ function App() {
 		}
 	};
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  //update the id of the new post to be the current datetime
-  setNewPost({...newPost, id: Date.now()})
-try {
-  //send a post request with newPost in the request body
-  await axios.post(`${BASE_URL}/posts`, newPost)
-  setPosts([...posts, newPost]);
-  //reset the new post state so that it is ready for another new post
-  setNewPost({id: 0, title: "", content: "", comments: []});
-} catch (error) {
-  console.error("Error adding post:", error);
-}
-}
-const handleEditPost = async (e) => {
-  try {
-    // send a put request with updated post data
-    await axios.put(`${BASE_URL}/posts/${editingPost.id}`, editingPost);
-    // update client side content
-    setPosts(posts.map((post) => (post.id === editingPost.id ? editingPost : post)));
-    // reset the editing post state to null
-    setEditingPost(null);
-  } catch (error) {
-    console.error("Error editing post", error);
+  const handleEditPost = async (e) => {
+		try {
+			// send a put request with updated post data
+			await axios.put(
+				`${BASE_URL}/posts/${editingPost.id}`,
+				editingPost
+			);
+			// update client side content
+			setPosts(
+				posts.map((post) =>
+					post.id === editingPost.id ? editingPost : post
+				)
+			);
+			// reset the editing post state to null
+			setEditingPost(null);
+		} catch (error) {
+			console.error('Error editing post', error);
+		}
+	};
+
+  // create submit function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setNewPost({...newPost, id: Date.now()})
+    try {
+      // send a post request with new post in the request body
+      await axios.post(`${BASE_URL}/posts`, newPost);
+      // update client side content
+      setPosts([...posts, newPost]);
+      // reset the new post state to blank
+      setNewPost({id: 0, title: '', content: '', comments: []});
+    } catch (error) {
+      console.error("Error adding post", error);
+    }
   }
-};
 
   return (
-    <div>
-      <h1>Blog Posts</h1>
-      <ul>
+		<div>
+			<h1>Blog Posts</h1>
+			<ul>
 				{posts.map((post) =>
-					editingPost == post.id ? (
-						<li key={post.id}>
-							<input type='text' placeholder={post.title} />
-							<textarea placeholder={post.content} />
+					(editingPost && editingPost.id === post.id) ? (
+						
+            <li key={post.id}>
+              {/*Need this to be a form*/}
+							<input 
+                type='text' 
+                placeholder={post.title}
+                value={editingPost.title} 
+              />
+							<textarea 
+                placeholder={post.content} 
+                value={editingPost.content}                
+              />
 							<button onClick={() => handleDelete(post.id)}>
 								Delete
 							</button>
-							<button onClick={() => setEditingPost(post.id)}>
-								Edit
-							</button>
+							<button onClick={() => handleEditPost()}>Edit</button>
 						</li>
 					) : (
 						<li key={post.id}>
@@ -79,32 +100,43 @@ const handleEditPost = async (e) => {
 							<button onClick={() => handleDelete(post.id)}>
 								Delete
 							</button>
-							<button onClick={() => setEditingPost(post.id)}>
+							<button onClick={() => setEditingPost(post)}>
 								Edit
 							</button>
 						</li>
 					)
 				)}
 			</ul>
-      {/* {posts ? JSON.stringify(posts, null, 2) : "Loading..."} */}
-       <form onSubmit={handleSubmit}>
-        <h3>Create new posts</h3>
-        <input
-        type='text'
-        placeholder='Title'
-        value={newPost.title}
-        onChange={(e) => setNewPost({...newPost, title: e.target.value})}
-       />
-       <textarea
-       placeholder='Content'
-       value={newPost.content}
-       onChange={(e) => setNewPost({...newPost, content: e.target.value})}
-       />
-       <button type='submit'>Add Post</button>
-       
-       </form>
-    </div>
-  );
+			<br />
+			<form onSubmit={handleSubmit} style={{ display: 'block' }}>
+				<h3>Create new post</h3>
+				<input
+					style={{ display: 'block', marginBottom: '10px' }}
+					type='text'
+					placeholder='Title...'
+					value={newPost.title}
+					// when the value in the input field changes, update the title property of the newPost state
+					onChange={(e) =>
+						setNewPost({ ...newPost, title: e.target.value })
+					}
+				/>
+				<textarea
+					style={{ display: 'block', marginBottom: '10px' }}
+					placeholder='Content...'
+					value={newPost.content}
+					onChange={(e) =>
+						setNewPost({ ...newPost, content: e.target.value })
+					}
+				/>
+				<button
+					type='submit'
+					style={{ display: 'block', marginBottom: '10px' }}
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
-export default App;
+export default App
